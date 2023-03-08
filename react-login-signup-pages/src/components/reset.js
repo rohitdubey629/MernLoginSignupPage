e06e7b128cadb09f014fcf8002ae5ef86017ac39
rohitdubey629 <dubeyrohit629@gmail.con>
2023-03-08 07:42:56 +0530

    First commit

---
+++ react-login-signup-pages/src/components/reset.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+export default class Reset extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { email } = this.state;
+    console.log(email);
+    fetch("http://localhost:5000/forgot-password", {
+      method: "post",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        email,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "userRegister");
+      });
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h3>Forgot Password</h3>
+
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+            onChange={(e) => this.setState({ email: e.target.value })}
+          />
+        </div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            submit
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          <a href="/sign-up">Sign Up</a>
+        </p>
+      </form>
+    );
+  }
+}
---
+++ react-login-signup-pages/src/components/userHome.js
@@ -0,0 +1,21 @@
+import React, { Component, useEffect, useState } from "react";
+// import UserDetails from "./userDetails";
+
+function UserHome({ userData }) {
+  const logOut = () => {
+    window.localStorage.clear();
+    window.location.href = "./sign-in";
+  };
+  return (
+    <div>
+      Name<h1>{userData.fname}</h1>
+      Email <h1>{userData.email}</h1>
+      <br />
+      <button onClick={logOut} className="btn btn-primary">
+        Log Out
+      </button>
+    </div>
+  );
+}
+
+export default UserHome;
